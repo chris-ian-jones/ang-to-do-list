cbe062edc0f75ffa7048f7809863e666e244c8fb
Chris Jones <chrisjones@Chriss-MacBook-Pro.local>
2019-09-16 01:52:26 +0100

    WIP: setup DELETE request to server and removing todo from UI

--- src/app/services/todo.service.ts
+++ src/app/services/todo.service.ts
@@ -4,6 +4,12 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 import { Todo } from './../models/Todo';
 
+const httpOptions = {
+  headers: new HttpHeaders({
+    'Content-Type': 'application/json'
+  })
+}
+
 @Injectable({
   providedIn: 'root'
 })
@@ -16,4 +22,14 @@ export class TodoService {
   getTodos():Observable<Todo[]> {
     return this.http.get<Todo[]>(`${this.todosUrl}${this.todosLimit}`);
   }
+
+  deleteTodo(todo:Todo):Observable<Todo> {
+    const url = `${this.todosUrl}/${todo.id}`;
+    return this.http.delete<Todo>(url, httpOptions)
+  }
+
+  toggleCompleted(todo: Todo):Observable<any> {
+    const url = `${this.todosUrl}/${todo.id}`;
+    return this.http.put(url, todo, httpOptions);
+  } 
 }
