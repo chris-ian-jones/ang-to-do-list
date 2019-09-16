e6a8359d0f90a4bf1856372d2ad7bcd5db016fe6
Chris Jones <chrisjones@Chriss-MacBook-Pro.local>
2019-09-16 01:19:37 +0100

    WIP: setup models

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ang-to-do-list';
+  title = 'Todo List';
+  days = ['monday', 'tuesday', 'wednesday', 'thursday', 'friday']
+  dates = [
+    {day: 'monday', date: '1st January', weather: 'sunny'},
+    {day: 'tuesday', date: '2nd January', weather: 'rainy'},
+    {day: 'wedsday', date: '3rd January', weather: 'cloudy'},
+    {day: 'thursday', date: '4th January', weather: 'thundery'},
+    {day: 'friday', date: '5th January', weather: 'lightningy'},
+    {day: 'saturday', date: '6th January', weather: 'snowy'},
+    {day: 'sunday', date: '7th January', weather: 'windy'},
+  ]
 }
